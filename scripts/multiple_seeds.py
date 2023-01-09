51e79a7426be00c062eccfb31c052143a1dffc6f
Sheng-Te Tsao <tsaost@gmail.com>
2023-01-08 22:32:05 -0500

    Add ability to generate all seeds between two given seeds.

--- scripts/multiple_seeds.py
+++ scripts/multiple_seeds.py
@@ -30,6 +30,7 @@ class Script(scripts.Script):
                     This script will only activate when one or more seeds are typed in this Seeds textbox.
                     <br>If there are no seeds entered, then images will be generated as normal, as if this script wasn't running.
                     <br>But if there are seeds entered, Batch count and Batch size above are ignored, and each seed will be generated one at a time.
+                    <br>If there is just two seeks, then do all the seeds between the two
                     </div>
                     """)
 
@@ -42,7 +43,10 @@ class Script(scripts.Script):
 
         seeds = []
         seeds = re.split(",| |\n", seed_textbox) #split by comma, space, or newline
-        seeds = list(filter(None, seeds)) #filter out any bad data created by multiple seperators in the textbox
+        seeds = list(filter(None, seeds)) # filter out any bad data created by multiple seperators in the textbox
+
+        if len(seeds) == 2 and seeds[0] < seeds[1]:
+            seeds = list(range(seeds[0], seeds[1]))
 
         if p.enable_hr:
             print(f"Will create {len(seeds)} images ({len(seeds)*2} jobs with Highres fix) with seeds: {seed_textbox}")
@@ -53,15 +57,12 @@ class Script(scripts.Script):
         p.n_iter = 1  # override users 'Batch count'
         p.batch_size = 1  # override users 'Batch size'
         images_list = []
-
         for seed in seeds:
             #print("Seed: ["+seed+"]\n")
-            shared.state.job_count = len(seeds) #for some reason, this line fixes the broken progress bar when using "Highres. fix" (p.enable_hr)
-
+            shared.state.job_count = len(seeds) # for some reason, this line fixes the broken progress bar when using "Highres. fix" (p.enable_hr)
             p.seed = seed.strip()
             proc = process_images(p)
             images_list += proc.images
-
         info_text = "Seeds:\n" + seed_textbox
         return Processed(p, images_list, seeds[0], info_text, None, None, seeds)
         #(StableDiffusionProcessing, images_list, seed = -1, info = "", subseed = None, all_prompts = None, all_seeds = None, all_subseeds = None, index_of_first_image = 0, infotexts = None):
